15dfb9392592109343c62edf5ca92a9380312514
DanieleMorello <danielemorello@live.it>
2023-04-13 18:13:06 +0200

    Add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,44 @@
 <script>
+import AppHeader from "./components/AppHeader.vue"
 export default {
-  data() {
-    return {
-
-    }
+  components: {
+    AppHeader
   }
 } 
 </script>
 
-<template class="debug">
-  <header></header>
-  <main></main>
-  <footer>
-    <div class="footer_top"></div>
-    <div class="footer_bottom"></div>
-  </footer>
+<template>
+  <div class="debug">
+    <AppHeader></AppHeader>
+    <main></main>
+    <footer>
+      <div class="footer_top"></div>
+      <div class="footer_bottom"></div>
+    </footer>
+  </div>
 </template>
 
 <style lang="scss">
 .debug {
   header {
     height: 122px;
-    background-color: b1b2b5;
+    background-color: #b1b2b5;
   }
 
   main {
     height: 129px;
-    background-color: 7e2b97;
+    background-color: #7e2b97;
   }
 
   footer {
     .footer_top {
       height: 365px;
-      background-color: 3a59b8;
+      background-color: #3a59b8;
     }
 
     .footer_bottom {
       height: 103px;
-      background-color: 841a56;
+      background-color: #841a56;
     }
   }
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+  data() {
+    return {
+      activeLink: 0
+
+    }
+  },
+  methods: {
+    setActiveLink(index) {
+      console.log(index);
+      this.activeLink = index;
+      console.log("Link attivo", this.activeLink);
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div>
+      <img src="../assets/img/dc-logo.png" alt="" class="logo">
+    </div>
+    <ul>
+      <li><a href=" #">characters</a></li>
+      <li><a href="#">comics</a></li>
+      <li><a href="#">movies</a></li>
+      <li><a href="#">tv</a></li>
+      <li><a href="#">games</a></li>
+      <li><a href="#">collectibles</a></li>
+      <li><a href="#">videos</a></li>
+      <li><a href="#">fans</a></li>
+      <li><a href="#">news</a></li>
+      <li><a href="#">shop</a></li>
+    </ul>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+$text-dark: #000000;
+
+header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  ul {
+    display: flex;
+
+    li {
+      padding: 1rem;
+      list-style: none;
+
+      a {
+        color: $text-dark;
+        text-transform: uppercase;
+        font-weight: bold;
+        text-decoration: none;
+      }
+    }
+  }
+}
+
+.active {
+  color: #0282f9;
+}
+</style>
